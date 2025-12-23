d4fa57cc3f07311f4daf621fd7dce58eacc1e8e3
dhineshwaran-s <kit28.24bcb013@gmail.com>
2025-12-23 12:10:45 +0530

    added dice project

---
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,20 @@
+//for image 1
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomImageSource = "./images/" + randomDiceImage;
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+//for image 2
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+//for heading  
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
